5bd216933fa8d9ef70f1c7851c06617368423aba
sophal <hongsophal@github.com>
2019-10-15 05:51:37 +0000

    Fix leading tabs error, updated

--- vim/golang-autocomplete/go_autocomplete.sh
+++ vim/golang-autocomplete/go_autocomplete.sh
@@ -23,51 +23,51 @@ if [ -f ~/.vimrc ]; then
     echo "backup ~/.vimrc [.vimrc_bak]"
     cp ~/.vimrc ~/.vimrc_bak
 else
-    cat > ~/.vimrc << EOF
-"==================Default setting===================
-colorscheme peachpuff   " awesome colorscheme
-"set background=dark
-syntax enable           " enable syntax processing
+    cat > ~/.vimrc <<-EOF
+	"==================Default setting===================
+	colorscheme peachpuff   " awesome colorscheme
+	"set background=dark
+	syntax enable           " enable syntax processing
 
-"Indentation without hard tabs :
-set expandtab           " tabs are spaces
-set softtabstop=4       " number of spaces in tab when editing
-set tabstop=4           " number of visual spaces per TAB
-set shiftwidth=4        " automatically indent next line (if, {, ...)
-"softtabstop, tabstop, shiftwidth should be the same value
+	"Indentation without hard tabs :
+	"set expandtab           " tabs are spaces
+	set softtabstop=4       " number of spaces in tab when editing
+	set tabstop=4           " number of visual spaces per TAB
+	set shiftwidth=4        " automatically indent next line (if, {, ...)
+	"softtabstop, tabstop, shiftwidth should be the same value
 
-set number              " show line numbers
-set showcmd             " show command in bottom bar
-set cursorline          " highlight current line
-set ruler               " Display line, column, virtual column number
-set wildmenu            " visual autocomplete for command menu
-set showmatch           " highlight matching [{()}]
-set incsearch           " search as characters are entered
-set hlsearch            " highlight matches
-set ignorecase          " Do case insenitive matching
-set smartcase           " Do smart case matching
-set foldenable          " enable folding
-set foldlevelstart=10   " open most folds by default
-set foldnestmax=10      " 10 nested fold max
-set foldmethod=indent   " fold based on indent level
-set textwidth=80        " wrap text at 80 characters
+	set number              " show line numbers
+	set showcmd             " show command in bottom bar
+	set cursorline          " highlight current line
+	set ruler               " Display line, column, virtual column number
+	set wildmenu            " visual autocomplete for command menu
+	set showmatch           " highlight matching [{()}]
+	set incsearch           " search as characters are entered
+	set hlsearch            " highlight matches
+	set ignorecase          " Do case insenitive matching
+	set smartcase           " Do smart case matching
+	set foldenable          " enable folding
+	set foldlevelstart=10   " open most folds by default
+	set foldnestmax=10      " 10 nested fold max
+	set foldmethod=indent   " fold based on indent level
+	set textwidth=80        " wrap text at 80 characters
 
-if has("syntax")
-   syntax on            " syntax highlighting
-endif
-if has("autocmd")       " Jump to last position when reopening file
-  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-endif
-if has("autocmd")       " Load indentation rules and plugins
-  filetype plugin indent on
-endif
+	if has("syntax")
+	   syntax on            " syntax highlighting
+	endif
+	if has("autocmd")       " Jump to last position when reopening file
+	  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+	endif
+	if has("autocmd")       " Load indentation rules and plugins
+	  filetype plugin indent on
+	endif
 
-highlight LineNr ctermfg=grey
-highlight Pmenu ctermfg=15 ctermbg=0 guifg=#ffffff guibg=#000000
-hi SpellBad ctermfg=yellow ctermbg=red
-hi SpellCap ctermfg=none ctermbg=red
-"==================End Default setting================
-EOF
+	highlight LineNr ctermfg=grey
+	highlight Pmenu ctermfg=15 ctermbg=0 guifg=#ffffff guibg=#000000
+	hi SpellBad ctermfg=yellow ctermbg=red
+	hi SpellCap ctermfg=none ctermbg=red
+	"==================End Default setting================
+	EOF
 fi
 
 # Install git if not yet installed
@@ -81,16 +81,20 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	ed --quiet ~/.vimrc << END
-1i
-call plug#begin('~/.vim/plugged')
-" Insert plugin here
-call plug#end()
-.
-w
-q
-END
-	fi
+fi
+
+# Add plug function into ~/.vimrc
+if ! grep -q "^call plug#begin('~/.vim/plugged')" ~/.vimrc; then
+	ed --quiet ~/.vimrc <<-END
+	1i
+	call plug#begin('~/.vim/plugged')
+	" Insert plugin here
+	call plug#end()
+	.
+	w
+	q
+	END
+fi
 
 PlugBeginLine=`awk '/call plug#begin/{print NR; exit}' ~/.vimrc`
 
@@ -100,13 +104,13 @@ if grep -q "^Plug 'fatih/vim-go'" ~/.vimrc; then
 else
     echo "Insert fatih/vim-go for PlugInstall..."
     PlugBeginLine=$((PlugBeginLine + 1))
-    ed --quiet ~/.vimrc << END
-${PlugBeginLine}i
-Plug 'fatih/vim-go' , { 'do': ':GoInstallBinaries' }
-.
-w
-q
-END
+    ed --quiet ~/.vimrc <<-END
+	${PlugBeginLine}i
+	Plug 'fatih/vim-go' , { 'do': ':GoInstallBinaries' }
+	.
+	w
+	q
+	END
 
     vim +slient +VimEnter +PlugInstall +qall
 fi
@@ -128,19 +132,19 @@ if ! [ -x "$(command -v yarn)" ]; then
     sudo apt-get update && sudo apt-get install yarn -y;
 fi
 
-# Install coc.nvim 
+# Install coc.nvim (Conquer of Completion)
 if grep -q "^Plug 'neoclide/coc.nvim'" ~/.vimrc; then
     echo "neoclide/coc.nvim is already installed!"
 else
     echo "Insert neoclide/coc.nvim for PlugInstall..."
     PlugBeginLine=$((PlugBeginLine + 1))
-    ed --quiet ~/.vimrc << END
-${PlugBeginLine}i
-Plug 'neoclide/coc.nvim', {'do': 'yarn install --frozen-lockfile'}
-.
-w
-q
-END
+    ed --quiet ~/.vimrc <<-END
+	${PlugBeginLine}i
+	Plug 'neoclide/coc.nvim', {'do': 'yarn install --frozen-lockfile'}
+	.
+	w
+	q
+	END
 
     vim +slient +VimEnter +PlugInstall +qall
 fi
@@ -150,17 +154,17 @@ if ! [ -x "$(command -v gopls)" ]; then
     echo "[ERROR] Cannot find 'gopls' in environment PATH !"
     exit;
 else
-    cat > ~/.vim/coc-settings.json << EOF
-{
-  "languageserver": {
-    "golang": {
-      "command": "gopls",
-      "rootPatterns": ["go.mod", ".vim/", ".git/", ".hg/"],
-      "filetypes": ["go"]
-    }
-  }
-}
-EOF
+    cat > ~/.vim/coc-settings.json <<-EOF
+	{
+	  "languageserver": {
+		"golang": {
+		  "command": "gopls",
+		  "rootPatterns": ["go.mod", ".vim/", ".git/", ".hg/"],
+		  "filetypes": ["go"]
+		}
+	  }
+	}
+	EOF
 fi
 
 #read -d '' VAR << EOF
@@ -188,7 +192,7 @@ set updatetime=300
 set shortmess+=c
 
 " always show signcolumns
-set signcolumn=yes
+"set signcolumn=yes
 
 " Use tab for trigger completion with characters ahead and navigate.
 " Use command ':verbose imap <tab>' to make sure tab is not mapped by other plugin.
