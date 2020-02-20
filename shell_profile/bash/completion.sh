95db12f1d686a572c3d25d4435f187dcc2150067
sophal <tech.sophal@gmail.com>
2020-02-20 14:43:27 +0900

    Add kubectx, kubens bash completion

--- shell_profile/bash/completion.sh
+++ shell_profile/bash/completion.sh
@@ -3,3 +3,6 @@
 [[ -x "$(command -v minikube)" ]] && source <(minikube completion bash)
 [[ -x "$(command -v kubectl)" ]] && source <(kubectl completion bash)
 [[ -x "$(command -v velero)" ]] && source <(velero completion bash)
+
+[[ -x "$(command -v kctx)" ]] && source $MY_BASH_DIR/completions/kubectx.bash
+[[ -x "$(command -v kns)" ]] && source $MY_BASH_DIR/completions/kubens.bash
