edf1e11658c0218b8dce40bea6fea5eb06ff46c0
sophal <tech.sophal@gmail.com>
2020-02-21 07:14:42 +0000

    Update bash completion: add helm completion

--- shell_profile/bash/completion.sh
+++ shell_profile/bash/completion.sh
@@ -3,6 +3,7 @@
 [[ -x "$(command -v minikube)" ]] && source <(minikube completion bash)
 [[ -x "$(command -v kubectl)" ]] && source <(kubectl completion bash)
 [[ -x "$(command -v velero)" ]] && source <(velero completion bash)
+[[ -x "$(command -v helm)" ]] && source <(helm completion bash)
 
 [[ -x "$(command -v kctx)" ]] && source $MY_BASH_DIR/completions/kubectx.bash
 [[ -x "$(command -v kns)" ]] && source $MY_BASH_DIR/completions/kubens.bash
