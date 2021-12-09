9d465b122e1359ee5ef78ec8d9b4b8bd2bd8d3a7
SOPHAL HONG <dev.sophal@gmail.com>
2021-12-09 16:24:20 +0900

    Update alias.sh

--- shell_profile/bash/alias.sh
+++ shell_profile/bash/alias.sh
@@ -13,7 +13,7 @@ alias vi='vim'
 alias ll='ls -alhF --color=auto'
 alias q='exit'
 
-alias chk_port_ltn='sudo lsof -i -P -n | grep LISTEN'
+alias chk_port_ltn='sudo lsof -i -P -n &> /dev/null | grep LISTEN || sudo ss -tulpn | grep LISTEN || sudo netstat -tulpn | grep LISTEN'
 alias generate_ctags='source ~/.my-terminal/vim/gentags_cscope'
 
 alias ssh_101='ssh -o TCPKeepAlive=yes -o ServerAliveInterval=360 192.168.0.101' #pw: root
