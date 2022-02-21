77498bec8e6f23a6811d744cb6cf346e02f88211
sophal <dev.sophal@gmail.com>
2022-02-21 16:35:00 +0900

    Add kubebuilder bash autocompletion

--- shell_profile/bash/completion.sh
+++ shell_profile/bash/completion.sh
@@ -5,19 +5,20 @@
 # gocomplete -install
 # gocomplete -uninstall
 
-[[ -x "$(command -v operator-sdk)" ]] && source <(operator-sdk completion bash)
 [[ -x "$(command -v golangci-lint)" ]] && source <(golangci-lint completion bash)
+[[ -x "$(command -v operator-sdk)" ]] && source <(operator-sdk completion bash)
+[[ -x "$(command -v kubebuilder)" ]] && source <(kubebuilder completion bash)
 [[ -x "$(command -v minikube)" ]] && source <(minikube completion bash)
 [[ -x "$(command -v kind)" ]] && source <(kind completion bash)
 [[ -x "$(command -v kubectl)" ]] && source <(kubectl completion bash)
 [[ -x "$(command -v kc)" ]] && source <(kc completion bash | sed -e 's/kubectl/kc/g')
-[[ -x "$(command -v velero)" ]] && source <(velero completion bash)
-[[ -x "$(command -v argo)" ]] && source <(argo completion bash)
-[[ -x "$(command -v k3d)" ]] && source <(k3d completion bash)
-[[ -x "$(command -v helm)" ]] && source <(helm completion bash)
-
 [[ -x "$(command -v kctx)" ]] && source $MY_BASH_DIR/completions/kubectx.bash
 [[ -x "$(command -v kns)" ]] && source $MY_BASH_DIR/completions/kubens.bash
+[[ -x "$(command -v k3d)" ]] && source <(k3d completion bash)
+
+[[ -x "$(command -v velero)" ]] && source <(velero completion bash)
+[[ -x "$(command -v argo)" ]] && source <(argo completion bash)
+[[ -x "$(command -v helm)" ]] && source <(helm completion bash)
 
 [[ -x "$(command -v VBoxManage)" ]] && source $MY_BASH_DIR/completions/VBoxManage.bash
 [[ -x "$(command -v goto)" ]] && source $MY_BASH_DIR/completions/goto.bash
